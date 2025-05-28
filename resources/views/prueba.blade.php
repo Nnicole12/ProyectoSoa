57a124885a60a13fc6db2f0a86d191555bbbc2b4
Nnicole12 <ynchicaiza1@espe.edu.ec>
2025-05-28 12:40:07 -0500

    Proyecto Laravel base funcionando

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Prueba Laravel</title>
+</head>
+<body>
+    <h1>¡Hola desde Laravel en la máquina de desarrollo (LINUX2)!</h1>
+    <p>Todo está funcionando correctamente ✅</p>
+</body>
+</html>
